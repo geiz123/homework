fc3a150920e86e07abaebf904251bb9d4e2bd59b
geiz123 <geiz123@users.noreply.github.com>
2015-12-25 23:00:50 -0500

    Added "IF EXIST" Added note in startH2servermode.bat for JDBC URL.

--- homeweb-sf/startH2servermode.bat
+++ homeweb-sf/startH2servermode.bat
@@ -1,3 +1,3 @@
 rem java -cp E:\Work\server\jboss-jpp-6.1.0\jboss-jpp-6.1\modules\system\layers\base\com\h2database\h2\main\h2-1.3.168-redhat-2.jar org.h2.tools.Server
-
+rem JDBC URL jdbc:h2:file:E:/H2Database/Garu
 java -cp E:\Work\server\jboss-eap-6.1\modules\system\layers\base\com\h2database\h2\main\h2-1.3.168-redhat-2.jar org.h2.tools.Server
